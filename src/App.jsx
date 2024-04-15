678efa2685bb63a74b42ec758570234991c613d2
iamvince24 <yohongguo24@gmail.com>
2024-04-16 00:54:11 +0800

    rebase testing 2

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./style.css";
 
 function App() {
-  return <h1 className="text-primary text-4xl font-bold">rebase testing 1</h1>;
+  return <h1 className="text-primary text-4xl font-bold">rebase testing 2</h1>;
 }
 
 export default App;
