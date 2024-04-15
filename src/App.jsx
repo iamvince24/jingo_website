1b84971eda57afeb21ff4b5611d2f59588d47221
iamvince24 <yohongguo24@gmail.com>
2024-04-15 23:08:58 +0800

    test sourcetree

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,7 @@ import React from "react";
 import "./style.css";
 
 function App() {
-  return (
-    <h1 className="text-primary text-4xl font-bold">
-      Hello world! I am using React
-    </h1>
-  );
+  return <h1 className="text-primary text-4xl font-bold">Using Sourcetree</h1>;
 }
 
 export default App;
