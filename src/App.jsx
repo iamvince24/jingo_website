a8d393124444383a54d96ad7cfa1f6e766fdb5df
iamvince24 <yohongguo24@gmail.com>
2024-04-15 23:19:02 +0800

    branch testing

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./style.css";
 
 function App() {
-  return <h1 className="text-primary text-4xl font-bold">Using Sourcetree</h1>;
+  return <h1 className="text-primary text-4xl font-bold">Branch testing</h1>;
 }
 
 export default App;
