6bc4424626071aacd762039778b8a3688dcbc3c0
iamvince24 <yohongguo24@gmail.com>
2024-09-05 16:24:38 +0800

    0905-1

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ function App() {
       <h1 className="text-primary text-4xl font-bold">Branch testing 4</h1>
       <h1 className="text-primary text-4xl font-bold">Branch test 0623</h1>
       <h1 className="text-primary text-4xl font-bold">Branch test 0905</h1>
+      <h1 className="text-primary text-4xl font-bold">Branch test 0905-1</h1>
     </div>
   );
 }
